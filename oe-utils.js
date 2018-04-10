67562e16bfcc4f6163e389606187acc086796531
Rohit Khode <Rohit.Khode@ad.infosys.com>
2018-04-10 14:16:53 +0530

    padStart the YYYY year

--- oe-utils.js
+++ oe-utils.js
@@ -90,6 +90,25 @@ if (!String.prototype.endsWith) {
   };
 }
 
+
+// https://github.com/uxitten/polyfill/blob/master/string.polyfill.js
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+if (!String.prototype.padStart) {
+    String.prototype.padStart = function padStart(targetLength,padString) {
+        targetLength = targetLength>>0; //truncate if number or convert non-number to 0;
+        padString = String((typeof padString !== 'undefined' ? padString : ' '));
+        if (this.length > targetLength) {
+            return String(this);
+        }
+        else {
+            targetLength = targetLength-this.length;
+            if (targetLength > padString.length) {
+                padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
+            }
+            return padString.slice(0,targetLength) + String(this);
+        }
+    };
+}
 OEUtils.geturl = OEUtils.geturl || function (url) {
 
   if (url.startsWith('http') ||
